e3b42473eb7a6d66bead86331c27f753765b3a68
BETAKOTLETA <viktor.oleniuk@gmail.com>
2025-03-20 16:50:25 +0200

    done

--- Utilities/Utilities/IntExtensions.cs
+++ Utilities/Utilities/IntExtensions.cs
@@ -2,5 +2,36 @@
 
 public static class IntExtensions
 {
-    //your code goes here
+    public static string ToOrdinal(this int value)
+    {
+        if (value % 100 >= 11 && value % 100 <= 13)
+        {
+            return value + "th";
+        }
+
+        switch (value % 10)
+        {
+            case 1:
+                return value + "st";
+            case 2:
+                return value + "nd";
+            case 3:
+                return value + "rd";
+            default:
+                return value + "th";
+        }
+    }
+    public static bool IsPrime(this int value)
+    {
+        if (value <= 1) return false; 
+        if (value == 2) return true; 
+        if (value % 2 == 0) return false;
+
+        for (int i = 3; i * i <= value; i += 2)
+        {
+            if (value % i == 0)
+                return false;
+        }
+        return true;
+    }
 }
--- Utilities/Utilities/StringExtensions.cs
+++ Utilities/Utilities/StringExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace Utilities;
+﻿using System.Text.RegularExpressions;
+
+namespace Utilities;
 
 public static class StringExtensions
 {
-    //your code goes here
+    public static string ToSnakeCase(this string input)
+    {
+        //return new string(input.Select(c => c == ' ' ? '_' : c).ToArray()).ToLower();
+
+        string cleaned = Regex.Replace(input, @"[^\w\s]", "");
+
+        return Regex.Replace(cleaned.Trim(), @"\s+", "_").ToLower();
+
+    }
 }
